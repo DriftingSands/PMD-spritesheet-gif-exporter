7e04027bc1fe44a1f77587c24e1fef9a5ec35c8f
Patrick <patrickanddaniel100@gmail.com>
2021-11-30 10:48:58 +0100

    added final adjustments, and exported pokemon sprites

--- make_gif.py
+++ make_gif.py
@@ -21,26 +21,31 @@ def make_gif_import(input_frames, input_frame_duration, name):
         width = width * 4
         height = height * 4
         new_frame = new_frame.resize((width, height), Image.NEAREST)
+        '''
+        extra for trying to get GIF to work... it just wont, I'm sticking wo WEBP
+
         alpha = new_frame.split()[3]
         new_frame = new_frame.convert('RGB').convert('P', palette=Image.ADAPTIVE)
         mask = Image.eval(alpha, lambda a: 255 if a <=128 else 0)
         new_frame.paste(255, mask)
+        '''
+
         for i in range(input_frame_duration[duration_index]):
             frames.append(new_frame)
         duration_index += 1
     # frames[1].show()
 
     path = pathlib.Path().resolve()
-    if name == '006':
-        print(input_frame_duration)
-        print(17*greatest_frame_div)
+    # if name == '006':
+    #     print(input_frame_duration)
+    #     print(17*greatest_frame_div)
 
     frames[0].save(f'{path}/output/{name}.webp', format='webp',
         save_all=True, append_images=frames[1:],
         duration=(17*greatest_frame_div), loop=0,
         disposal=2,
         transparency=255,
-        # background=(255, 0, 255, 0),
+        # background=(255, 0, 255, 255),
         # version='GIF89a',
     )
     # new_gif = Image.open(f'{path}/output/{name}.webp')
--- run.py
+++ run.py
@@ -4,8 +4,15 @@ import glob
 
 pokemon_dir = glob.glob('pmd_monster/*/')
 
+
 for element in pokemon_dir:
-    name = element[12:15]
+    name = element.split('pmd_monster\\')[1]
+    name = name.split('_')
+    if len(name) == 2:
+        name = name[0][0:3] + '_' + name[1]
+        name = name[:-1]
+    else:
+        name = name[0][0:3]
 
     # pass (animation, direction, directory)
     frames, frame_duration = cut_png.cut_png(1, 7, element)
